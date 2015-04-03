0b2c3d181fd6c6861e32539ade0abdc308c8c32d
grantziolkowski & Kenny <pair+grantziolkowski.kennygeiler@devbootcamp.com>
2015-04-03 17:07:25 -0400

    With directions to bins

--- recycle.rb
+++ recycle.rb
@@ -57,14 +57,25 @@ class BinController
     result = @recycling_bins_array.map do |recycling_bin|
       @borough = recycling_bin[:borough]
       call = JSON.parse(open("https://maps.googleapis.com/maps/api/geocode/json?latlng=#{recycling_bin[:latitude]},#{recycling_bin[:longitude]}").read)
+      nav = navigation(recycling_bin[:latitude], recycling_bin[:longitude])
       inner_hash = call.access("results.0")
-      inner_hash.access("formatted_address").split(",")[0]
+      address = inner_hash.access("formatted_address").split(",")[0]
+      "#{address}\n#{nav}\n"
     end.join("\n")
     puts
     puts "These are the recycling bins within approximately half a mile of you in #{@borough}:"
     puts
     puts result
   end
+
+  def navigation(dest_lat, dest_lng)
+  directions = JSON.parse(open("https://maps.googleapis.com/maps/api/directions/json?origin=#{@kenny_lat},#{@kenny_long}&destination=#{dest_lat},#{dest_lng}&mode=walking").read)
+
+duration = directions.access("routes.0.legs.0.steps.0.distance.text")
+
+exact_direction = directions.access("routes.0.legs.0.steps.0.html_instructions")
+navi = "Walk #{duration}, and #{exact_direction}".gsub(/<\/?[^>]+>/, '')
+end
 
   def run
     get_coordinates
